13142f27f6aff6b267d92888ade4edaf9c842dc5
Drew Cooper <drewcoo@gmail.com>
2020-05-25 01:14:58 -0700

    tire tread
    Cheesy and probably done very badly, but . . . tire tread!

--- client/src/components/combat/overlays/vehicle/modal/carInset/carComponents/Tire.tsx
+++ client/src/components/combat/overlays/vehicle/modal/carInset/carComponents/Tire.tsx
@@ -31,9 +31,9 @@ class Tire extends React.Component<Props> {
   tireStyle(): any {
     if (this.props.tireData.damagePoints >= 1) {
       return {
-        fill: 'darkslategray',
+        fill: 'black',
         stroke: 'black',
-        strokeWidth: 2,
+        strokeWidth: 1,
       }
     }
     if (this.props.tireData.wheelExists) {
@@ -50,10 +50,18 @@ class Tire extends React.Component<Props> {
     }
   }
 
+  treadStyle(): any {
+    return {
+      fill: 'url(#pattern-chevron)',
+        stroke: '#000000',
+        strokeWidth: 1, 
+    }
+  }
+
   render(): React.ReactNode {
     const x = this.left()
       ? 0.5 * GenericComponent.dimensions().width
-      : this.props.carDimensions.width - 1.15 * GenericComponent.dimensions().width
+      : this.props.carDimensions.width - 1.25 * GenericComponent.dimensions().width
     const y = this.front()
       ? 2.5 * GenericComponent.dimensions().height
       : this.props.carDimensions.height - 3 * GenericComponent.dimensions().height
@@ -65,7 +73,25 @@ class Tire extends React.Component<Props> {
     const centerY = y + height / 2 + boxSide * ((this.props.tireData.maxDamagePoints / 7) % 2) + boxSide / 2
 
     return (
-      <g>
+      <svg>
+        <defs>
+          <pattern
+            id="pattern-chevron"
+            x="0"
+            y="0"
+            patternUnits="userSpaceOnUse"
+            width="5"
+            height="7.5"
+            viewBox="0 0 10 10"
+          >
+            <g id="chevron">
+              <path className="left" d="M0 0l5 3v5l-5 -3z" fill='#222222' />
+              <path className="right" d="M10 0l-5 3v5l5 -3" fill='#222222' />
+            </g>
+            <use x="0" y="9" href="#chevron" />
+          </pattern>
+        </defs>
+
         <rect
           rx={this.props.carDimensions.width / 32}
           x={x}
@@ -73,6 +99,15 @@ class Tire extends React.Component<Props> {
           width={width * 1}
           height={height}
           style={this.tireStyle()}
+        />
+
+        <rect
+          rx={this.props.carDimensions.width / 32}
+          x={x}
+          y={y}
+          width={width * 1}
+          height={height}
+          style={this.treadStyle()}
         />
         <DamageBoxes
           tire={true}
@@ -82,7 +117,7 @@ class Tire extends React.Component<Props> {
           dp={this.props.tireData.damagePoints}
           maxDp={this.props.tireData.maxDamagePoints}
         />
-      </g>
+      </svg>
     )
   }
 }
